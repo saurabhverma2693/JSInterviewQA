961aad41fa9a56fe458eba1fae675c92111f134e
saurabh <saurabhverma2693@gmail.com>
2025-03-05 12:36:35 +0530

    Complete Deep Copy

--- DeepShallowClone.js
+++ DeepShallowClone.js
@@ -1,3 +1,10 @@
+// In JavaScript, shallow copy and deep copy refer to how objects or arrays are copied and how nested structures within them are handled.
+
+// Shallow Copy:
+// A shallow copy creates a new object or array, but it only copies the properties or elements at the first level.
+//  If the original object or array contains nested objects or arrays, 
+// the shallow copy will reference the same nested structures, not duplicate them.
+
 // Problem with Shallow Cloning:
 
 // It does not create copies of nested objects.
@@ -27,9 +34,24 @@
 
 //.................................................................................................................................................
 
-// DeepCopy
+
+// Deep Copy:
+// A deep copy creates a completely independent copy of the object or array, 
+// including all nested objects or arrays. Changes to the original structure
+//  do not affect the deep copy and vice versa.
+
+// DeepCopy Example:
 // let Deepcopy = JSON.parse(JSON.stringify(OrignalCopy));
 // Deepcopy[0][0] = 99;
 // console.log(OrignalCopy);
 // console.log(Deepcopy);
+
+// Shallow copy: Copies only the first level of properties. Nested structures are shared.
+// Deep copy: Copies all levels of properties. Nested structures are duplicated.
+
+
+
+
+
+
 
