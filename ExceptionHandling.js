e62928977d286f81971f35ba15076e7745502353
saurabh <saurabhverma2693@gmail.com>
2025-03-20 17:06:50 +0530

    Exception Handling

---
+++ ExceptionHandling.js
@@ -0,0 +1,11 @@
+// Exception Handling:
+// Exception handling in JavaScript is a mechanism to handle runtime errors gracefully without breaking the execution of the entire program.
+// It allows developers to catch and manage errors effectively.
+
+// try {
+//     // Code that may throw an exception
+// } catch (error) {
+//     // Code to handle the exception
+// } finally {
+//     // Code that always executes (optional)
+// }
