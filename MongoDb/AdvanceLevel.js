609c6fcee63a13039c98005881a6e9183e4fed11
saurabh <saurabhverma2693@gmail.com>
2025-05-24 15:22:38 +0530

    Advance Level

---
+++ MongoDb/AdvanceLevel.js
@@ -0,0 +1,40 @@
+// 🔴 Advanced-Level Questions
+// 11. What is sharding in MongoDB?
+// Sharding is horizontal scaling by distributing data across multiple servers.
+
+//............................................................................................................................................
+// 12. What is replication in MongoDB?
+// Replication is the process of copying data to multiple servers (Replica Set) for high availability.
+
+//............................................................................................................................................
+// 13. What are transactions in MongoDB?
+// Since v4.0, MongoDB supports multi-document ACID transactions, mostly used in replica sets.
+
+//............................................................................................................................................
+// 14. What is a capped collection?
+// A fixed-size collection that automatically overwrites oldest entries when it reaches size limit.
+
+//............................................................................................................................................
+// 15. What is the difference between MongoDB and Mongoose?
+// MongoDB is the database.
+
+// Mongoose is an ODM (Object Data Modeling) library for Node.js, providing schema, validation, and middleware.
+
+
+//............................................................................................................................................
+
+
+// ⚡ Bonus Practical Questions
+// How do you ensure unique values in a field?
+// ➤ Create a unique index.
+
+// How do you paginate data in MongoDB?
+// ➤ Use skip() and limit() or $facet in aggregation.
+
+// How to perform a full-text search?
+// ➤ Create a text index and use $text.
+
+// How do you back up a MongoDB database?
+// ➤ Use mongodump and mongorestore.
+
+//............................................................................................................................................
