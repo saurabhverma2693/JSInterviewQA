92c7d96fcb16ee7b08254b470be2e367c753af5f
saurabh <saurabhverma2693@gmail.com>
2025-05-24 15:18:32 +0530

    Beginners in mongodb

---
+++ MongoDb/Beginner.js
@@ -0,0 +1,43 @@
+// 🟢 Beginner-Level Questions
+// 1. What is MongoDB?
+// MongoDB is a NoSQL, document-oriented database that stores data in JSON-like BSON documents instead of rows and columns.
+
+//............................................................................................................................................
+
+// 2. Difference between SQL and MongoDB?
+// SQL (Relational)	MongoDB (NoSQL)
+// Tables and rows	Collections and documents
+// Schema required	Schema-less / dynamic
+// JOINs supported	No joins (but $lookup)
+// ACID transactions	Available (since v4.0)
+
+
+//............................................................................................................................................
+
+// 3. What is a document in MongoDB?
+// A document is a key-value pair data structure stored in BSON format.
+
+// Example:
+
+// json
+
+// {
+//   "_id": 1,
+//   "name": "Alice",
+//   "age": 25
+// }
+
+
+//............................................................................................................................................
+
+// 4. What is a collection in MongoDB?
+// A collection is a group of documents, similar to a table in relational databases.
+
+
+//............................................................................................................................................
+
+// 5. What is the _id field?
+// Every document has a unique _id field which acts as the primary key.
+
+
+//............................................................................................................................................
