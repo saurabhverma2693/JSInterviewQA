39d6e0ea9879763887d4d8e2b4bc5421c387cd4f
saurabh <saurabhverma2693@gmail.com>
2025-07-11 15:12:50 +0530

    Diff between Testing & Debugging

---
+++ NodeJs/TestingNDebugApp.js
@@ -0,0 +1,31 @@
+Diff between testing and dubugging in web applications:
+
+
+# Yes, there is a significant difference between testing and debugging 
+in web applications: testing aims to identify defects, 
+while debugging focuses on finding and fixing those defects.
+ Testing is about verifying functionality, performance, and reliability, 
+ while debugging involves the process of
+ locating and resolving issues once they've been found.
+
+
+# Key Differences:
+Purpose:
+Testing seeks to find bugs, errors,
+ and vulnerabilities to ensure the web application meets its requirements. 
+ Debugging's purpose is to pinpoint the exact cause of these identified issues and eliminate them. 
+
+ Process:
+Testing can be manual or automated and involves various types like 
+unit, integration, and performance testing. Debugging, on the other hand,
+ typically involves analyzing code, using tools like logging and tracing,
+  and modifying the source code to correct errors. 
+Who performs it:
+Testers are primarily responsible for performing tests, 
+while developers and programmers are typically responsible for debugging. 
+
+Timing:
+Debugging occurs after testing has identified a defect.
+ Testing can occur throughout the development lifecycle,
+ including before and after debugging sessions. 
+
